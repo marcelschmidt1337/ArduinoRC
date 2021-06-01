fa24f8e8b1f6a51935546690c725659ed788aeb4
Marcel Schmidt <github@m-schmidt.org>
2021-06-01 21:01:37 +0200

    Renamed main.ino to remote_control.ino

--- remote_control.ino
+++ remote_control.ino
@@ -2,18 +2,20 @@
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
 
-#include "src/state.h"
+#include "src/dooraction.h"
+#include "src/sensor.h"
 #include "src/const.h"
 #include "src/html.h"
 #include "src/helper.h"
 
 ESP8266WebServer server(80);
+DoorAction doorAction;
+SensorState desiredState = SensorState::Undefined;
 
 void renderHtml();
 void redirectToRoot();
 void handleApi();
-
-DoorState getDoorState();
+SensorState getDoorState();
 
 void setup()
 {
@@ -28,8 +30,12 @@ void setup()
     digitalWrite(DOOR_CONTROL, OFF);
     digitalWrite(SENSOR_ENABLE, OFF);
 
+    SensorState initialState = getDoorState();
+    desiredState = initialState;
+    doorAction.setAction(initialState);
+
     Serial.print("Setting soft-AP ... ");
-    boolean result = WiFi.softAP(SSID, PW);
+    bool result = WiFi.softAP(SSID, PW);
     if (result == true)
     {
         Serial.println("Ready");
@@ -68,16 +74,26 @@ void renderHtml()
     String content;
     content.reserve(1024);
 
-    DoorState state = getDoorState();
+    SensorState state = getDoorState();
 
-    String btnName = helper::getBtnActionString(state);
+    if (state != SensorState::Undefined)
+    {
+        doorAction.setAction(state);
+    }
+
+    String btnName = doorAction.toString();
     btnName.toUpperCase();
 
     content = html::begin_html() +
               html::h1("Remote Control") +
               html::p("Door: " + helper::getStateString(state)) +
-              html::button_post(btnName, "door", helper::getBtnActionString(state), "/api") +
+              html::button_post(btnName, "door", doorAction.toString(), "/api") +
               html::end_html();
+
+    if(state != desiredState)
+    {
+        server.sendHeader("Refresh", String(REFRESH_SECONDS));
+    }
 
     server.send(200, "text/html", content);
 }
@@ -90,16 +106,21 @@ void redirectToRoot()
 
 void handleApi()
 {
+    // Tor geht auf -> stop -> geht zu
+    // Tor geht zu -> kurz stop dann geht auf
     if (server.hasArg("door"))
     {
         digitalWrite(DOOR_CONTROL, ON);
         delay(100);
         digitalWrite(DOOR_CONTROL, OFF);
+
+        doorAction.updateAction();
+        desiredState = helper::getStateFromBtnAction(server.arg("door"));
     }
     redirectToRoot();
 }
 
-DoorState getDoorState()
+SensorState getDoorState()
 {
     digitalWrite(SENSOR_ENABLE, ON);
     bool isOpenSensorTriggered = helper::getBool(digitalRead(SENSOR_OPENED));
@@ -108,14 +129,14 @@ DoorState getDoorState()
 
     if (isOpenSensorTriggered && !isClosedSensorTriggered)
     {
-        return DoorState::Opened;
+        return SensorState::Opened;
     }
     else if (!isOpenSensorTriggered && isClosedSensorTriggered)
     {
-        return DoorState::Closed;
+        return SensorState::Closed;
     }
     else
     {
-        return DoorState::Undefined;
+        return SensorState::Undefined;
     }
 }
